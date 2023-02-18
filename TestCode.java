fe879ba8c2879d163cc2b975c799a55000026c0f
carevilla <124191778+carevilla@users.noreply.github.com>
2023-02-18 11:01:30 -0700

    Add files via upload
    Updated code

---
+++ TestCode.java
@@ -0,0 +1,21 @@
+package TestCode;
+/**
+ * 
+ * @author Christian Revilla
+ *
+ */
+
+public class TestCode {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hello Mundo");
+		System.out.println("New Comment");
+		
+		int a = 5;
+		int b = 10;
+		System.out.println(a+b);
+		System.out.println(b-a);
+	}
+
+}
